c9ddab196af5a8adb0b348eb38de939572c81ef9
Lauren Moor <laurenkaede@gmail.com>
2020-03-29 15:42:03 +0100

    Updates

---
+++ precache-manifest.3ed879d3cab9f673e6d96b3e5473cd08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5892446833073f66db769f848aa8deb",
+    "url": "/FirstReactProject/index.html"
+  },
+  {
+    "revision": "d28bf0cc3e1270168a95",
+    "url": "/FirstReactProject/static/css/main.d4d0d49d.chunk.css"
+  },
+  {
+    "revision": "dee0f0c06ec672bd17a4",
+    "url": "/FirstReactProject/static/js/2.f71a865a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FirstReactProject/static/js/2.f71a865a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d28bf0cc3e1270168a95",
+    "url": "/FirstReactProject/static/js/main.af53cbc5.chunk.js"
+  },
+  {
+    "revision": "53e383690a541dc32b55",
+    "url": "/FirstReactProject/static/js/runtime-main.689ce106.js"
+  },
+  {
+    "revision": "91f0324e98f4f9d0a0f51634137709d6",
+    "url": "/FirstReactProject/static/media/bella.91f0324e.jpg"
+  },
+  {
+    "revision": "741b723acba1cb9f23744c454b59e52c",
+    "url": "/FirstReactProject/static/media/daisy.741b723a.jpg"
+  }
+]);
